cc3551d501105b2c7d6b9bf2230f6246e668a58a
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-03-19 15:43:35 +0200

    added pug

--- server-two/app.js
+++ server-two/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.set('view engine', 'pug')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+
+});
 
 app.get('/catinfo', (req, res) => {
     const cat = {
